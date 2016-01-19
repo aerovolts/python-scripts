96f9f53a2bd8a7fcaf809b8df55c8c3cf08debae
Patrick Guelcher <aeroblitz@pwpg.ca>
2016-01-18 21:26:08 -0500

    Replace tabs with spaces
    Forgot to fix this in the previous commit.

--- md5bot.py
+++ md5bot.py
@@ -22,26 +22,26 @@ sleep_time = 3600 # Time is in seconds (Default 3600)
 
 # Do not edit beyond this comment
 def main():
-	index = 0
-	while index == 0:
-		post_status()
-		time.sleep(sleep_time) # Sleep for one hour
+    index = 0
+    while index == 0:
+        post_status()
+        time.sleep(sleep_time) # Sleep for one hour
 
 def check_time():
-	time_stamp = str(time.time())
-	encode_time = time_stamp.encode('utf-8')
-	md = hashlib.md5()
-	md.update(encode_time)
-	return md.hexdigest()
+    time_stamp = str(time.time())
+    encode_time = time_stamp.encode('utf-8')
+    md = hashlib.md5()
+    md.update(encode_time)
+    return md.hexdigest()
 
 def post_status():
-	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-	auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-	api = tweepy.API(auth)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    api = tweepy.API(auth)
 
-	current_time = check_time()
-	api.update_status(current_time)
-	print(current_time)
+    current_time = check_time()
+    api.update_status(current_time)
+    print(current_time)
 
 if __name__ == '__main__':
     main()
