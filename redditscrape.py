e944c42929ea81749ba24ade4e48ee6ada9ebbae
Patrick Guelcher <fortesp15@gmail.com>
2016-03-03 20:15:22 -0500

    Fix various directory issues

--- redditscrape.py
+++ redditscrape.py
@@ -3,7 +3,7 @@
 __author__ = "Patrick Guelcher"
 __copyright__ = "(C) 2016 Patrick Guelcher"
 __license__ = "MIT"
-__version__ = "3.0"
+__version__ = "3.1"
 
 """
 Scrapes the list of provided subreddits for images and downloads them to a local directory
@@ -13,18 +13,25 @@ import os, praw, wget, urllib.error
 
 # Configuration
 root_path = 'scrape' # Download folder (Default: scrape)
-sub_list = ['vexillology', 'mapporn', 'pics'] # Subreddit list
-post_limit = 15 # Sumbission limit to check and download
-user_agent = 'Image Scraper 3.0 by /u/aeroblitz' # Use your own reddit username
+sub_list = [
+            'vexillology',
+            'mapporn',
+            'pics',
+            'wallpapers'
+            ] # Subreddit list
+post_limit = 10 # Sumbission limit to check and download
+user_agent = 'Image Scraper 3.1 by /u/aeroblitz' # Use your own reddit username
 
 # Do not edit beyond this comment
 def main():
     create_folders()
 
 def create_folders():
-    os.mkdir(root_path)
+    os.makedirs(root_path, exist_ok=True)
     for sub in sub_list:
-        os.mkdir(os.path.join(root_path,str(sub)))
+        os.makedirs(os.path.join(root_path,str(sub)), exist_ok=True)
+#    else:
+#        print("DirectoryError: Could not create directory for " + sub)
     download_images()
 
 def download_images():
@@ -32,7 +39,7 @@ def download_images():
 
     for sub in sub_list:
         post_list = u.get_subreddit(sub).get_hot(limit=post_limit)
-        path = root_path + '/' + sub
+        download_path = root_path + '/' + sub
         for post in post_list:
             if post.url is not None:
                 file_name = post.url
@@ -40,9 +47,9 @@ def download_images():
                 if extension == '.jpg' or extension == '.png':
                     print ("\n" + post.url)
                     try:
-                        wget.download(post.url, path)
+                        wget.download(post.url, download_path)
                     except (IndexError, urllib.error.HTTPError):
-                        print ("\n" + "Error Downloading -- Skipping Image")
+                        print ("\n" + "DownloadError: Skipping file download.")
                         pass
                 else:
                     pass
