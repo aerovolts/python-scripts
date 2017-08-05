bd62ece59dbd96bdef4a3b10f94882ec09b5c41e
Patrick Guelcher <patrick.guelcher@gmail.com>
2017-08-05 12:38:37 -0400

    Added my FizzBuzz solution

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+# "Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+# For numbers which are multiples of both three and five print “FizzBuzz”
+
+for n in range(1, 101):
+    x = 3 # To quickly change that number shows 'Fizz'
+    y = 5 # To quickly change that number shows 'Buzz'
+    if n % x == 0 and n % y == 0:
+        print("FizzBuzz")
+    elif n % x == 0:
+        print ("Fizz")
+    elif n % y == 0:
+        print("Buzz")
+    else:
+        print(n)
